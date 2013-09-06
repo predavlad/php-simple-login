0099de35fcfda1b3c7d3dcaa97d36dc4d7ed945b
predavlad <preda.vlad@yahoo.com>
2013-09-06 11:25:10 +0300

    removed auto increment

--- db_user.sql
+++ db_user.sql
@@ -4,4 +4,4 @@ CREATE TABLE IF NOT EXISTS `users` (
   `pass` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `user` (`user`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
